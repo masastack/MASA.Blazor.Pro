a9800223e47b5dcfef2b5a51dbe854555572f9b0
阎鹏举 <yanpengju@masastack.com>
2021-12-22 03:51:38 +0000

    Fix/todo:修改todo页面已知bug

---
+++ MASA.Blazor.Pro/Demo/1-Apps/Todo/TodoDetail.razor.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace MASA.Blazor.Pro.Demo;
+
+public partial class TodoDetail
+{
+    private readonly List<SelectData> _tagList = new()
+    {
+        new SelectData() { Label = "Team", Value = "Team" },
+        new SelectData() { Label = "Low", Value = "Low" },
+        new SelectData() { Label = "Medium", Value = "Medium" },
+        new SelectData() { Label = "High", Value = "High" },
+        new SelectData() { Label = "Update", Value = "Update" }
+    };
+    private readonly List<SelectData> _assigneeList = new()
+    {
+        new SelectData() { Label = "紫萱", Value = "紫萱" },
+        new SelectData() { Label = "若芹", Value = "若芹" },
+        new SelectData() { Label = "思菱", Value = "思菱" },
+        new SelectData() { Label = "向秋", Value = "向秋" },
+        new SelectData() { Label = "雨珍", Value = "雨珍" },
+        new SelectData() { Label = "海瑶", Value = "海瑶" },
+        new SelectData() { Label = "乐萱", Value = "乐萱" },
+    };
+    private MForm? _mForm;
+    private bool _isEdit;
+    private TodoData _selectData = new();
+
+    private string CompletedColor { get { return _selectData.IsCompleted ? "theme--dark pry" : "text-capitalize neutral-lighten-5 neutral-lighten-2--text"; } }
+
+    private string CompletedText { get { return _selectData.IsCompleted ? "Mark Complete" : "Completed"; } }
+
+    [CascadingParameter]
+    public TodoList TodoList { get; set; } = default!;
+
+    [Parameter]
+    public bool Value { get; set; }
+
+    [Parameter]
+    public TodoData? SelectItem { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> ValueChanged { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
+    private async Task HideNavigationDrawer()
+    {
+        if (ValueChanged.HasDelegate)
+        {
+            await ValueChanged.InvokeAsync(false);
+        }
+    }
+
+    private void HandleCloseClick(string lable)
+    {
+        _selectData.Tag.Remove(lable);
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (SelectItem == null)
+        {
+            SelectItem = new TodoData();
+            _isEdit = false;
+        }
+        else
+        {
+            _isEdit = true;
+        }
+
+        _selectData = new TodoData
+        {
+            Id = SelectItem.Id,
+            IsChecked = SelectItem.IsChecked,
+            Assignee = SelectItem.Assignee,
+            Avatar = SelectItem.Avatar,
+            Description = SelectItem.Description,
+            IsCompleted = SelectItem.IsCompleted,
+            IsDeleted = SelectItem.IsDeleted,
+            IsImportant = SelectItem.IsImportant,
+            DueDate = SelectItem.DueDate,
+            Title = SelectItem.Title
+        };
+        _selectData.Tag.AddRange(SelectItem.Tag);
+
+        if (ValueChanged.HasDelegate && !Value && _mForm != null)
+        {
+            await _mForm.ResetValidationAsync();
+        }
+    }
+
+    private async Task Add(EditContext context)
+    {
+        var success = context.Validate();
+        if (success)
+        {
+            _selectData.Id = TodoService.List.Count + 1;
+            TodoService.List.Insert(0, _selectData);
+            await HideNavigationDrawer();
+
+            NavigationManager.NavigateTo("apps/todo");
+        }
+    }
+
+    private async Task Update(EditContext context)
+    {
+        var success = context.Validate();
+        if (success)
+        {
+            var data = (TodoData)context.Model;
+            TodoList.UpdateData(data);
+            await HideNavigationDrawer();
+        }
+    }
+
+    private void Reset()
+    {
+        if (SelectItem != null)
+        {
+            _selectData = new TodoData
+            {
+                Id = SelectItem.Id,
+                IsChecked = SelectItem.IsChecked,
+                Assignee = SelectItem.Assignee,
+                Avatar = SelectItem.Avatar,
+                Description = SelectItem.Description,
+                IsCompleted = SelectItem.IsCompleted,
+                IsDeleted = SelectItem.IsDeleted,
+                IsImportant = SelectItem.IsImportant,
+                DueDate = SelectItem.DueDate,
+                Tag = SelectItem.Tag,
+                Title = SelectItem.Title
+            };
+        }
+    }
+
+    private async Task DeleteTask()
+    {
+        _selectData.IsDeleted = true;
+        TodoList.UpdateData(_selectData);
+        await HideNavigationDrawer();
+    }
+}
---
+++ MASA.Blazor.Pro/Demo/1-Apps/Todo/TodoList.razor.cs
@@ -0,0 +1,118 @@
+﻿namespace MASA.Blazor.Pro.Demo;
+
+public partial class TodoList
+{
+    
+    private readonly Dictionary<string, string> _chipTextColor = new()
+    {
+        { "Team", "purple" },
+        { "Low", "teal" },
+        { "Medium", "orange" },
+        { "High", "red" },
+        { "Update", "cyan" },
+    };
+    private readonly Dictionary<string, string> _chipColor = new()
+    {
+        { "Team", "purple lighten-4" },
+        { "Low", "teal lighten-4" },
+        { "Medium", "orange lighten-4" },
+        { "High", "red lighten-4" },
+        { "Update", "cyan lighten-4" },
+    };
+    private readonly string[] _avas = new string[]
+    {
+        "https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/demo-1/img/5.f13458cc.png",
+        "https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/demo-1/img/1.9cba4a79.png",
+        "https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/demo-1/img/13-small.d796bffd.png",
+        "https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/demo-1/img/12.03bf9466.png",
+        "https://cdn.vuetifyjs.com/images/lists/4.jpg"
+    };
+    private TodoData _selectItem = new();
+    private string? _filterText;
+    private bool _visible = false;
+    private string? _inputText;
+    private List<TodoData> _thisList = new();
+    private readonly List<TodoData> _dataList = TodoService.List;
+
+    [Parameter]
+    public string? FilterText
+    {
+        get { return _filterText; }
+        set
+        {
+            _filterText = value;
+            _thisList = _filterText switch
+            {
+                "important" => _dataList.Where(item => item.IsImportant && !item.IsDeleted).ToList(),
+                "completed" => _dataList.Where(item => item.IsCompleted && !item.IsDeleted).ToList(),
+                "deleted" => _dataList.Where(item => item.IsDeleted).ToList(),
+                "team" => _dataList.Where(item => item.Tag.Contains("Team")).ToList(),
+                "low" => _dataList.Where(item => item.Tag.Contains("Low")).ToList(),
+                "medium" => _dataList.Where(item => item.Tag.Contains("Medium")).ToList(),
+                "high" => _dataList.Where(item => item.Tag.Contains("High")).ToList(),
+                "update" => _dataList.Where(item => item.Tag.Contains("Update")).ToList(),
+                _ => _dataList.Where(item => !item.IsDeleted).ToList(),
+            };
+        }
+    }
+
+    private void ShowDetail(TodoData item)
+    {
+        _visible = true;
+        _selectItem = item;
+    }
+
+    private void ResetSort()
+    {
+        _thisList = _thisList.OrderBy(d => d.Id).ToList();
+    }
+
+    private void SortbyAssignee()
+    {
+        _thisList = _thisList.OrderBy(d => d.Assignee).ToList();
+    }
+
+    private void SortbyDueDate()
+    {
+        _thisList = _thisList.OrderBy(d => d.DueDate).ToList();
+    }
+
+    private void InputTextChanged(string? text)
+    {
+        if (!string.IsNullOrWhiteSpace(text))
+            _thisList = _dataList.Where(item => item.Title.Contains(text)).ToList();
+        else
+            _thisList = _dataList;
+    }
+
+    public string? InputText
+    {
+        get { return _inputText; }
+        set
+        {
+            _inputText = value;
+            InputTextChanged(_inputText);
+        }
+    }
+
+    public void UpdateData(TodoData data)
+    {
+        var index = _dataList.FindIndex(d => d.Id == data.Id);
+        _dataList[index] = data;
+
+        FilterText = _filterText;
+    }
+
+    public void AddData(TodoData data)
+    {
+        _dataList.Insert(0, data);
+    }
+}
+
+public class SelectData
+{
+    public string Label { get; set; } = default!;
+
+    public string Value { get; set; } = default!;
+}
+
