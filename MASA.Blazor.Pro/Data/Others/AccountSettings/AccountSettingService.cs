51b50d0603a1a3955b04bbd10b304dbf666112a0
马跃 <mayue@masastack.com>
2022-02-18 04:29:18 +0000

    Feature/fix pagetabs

--- MASA.Blazor.Pro/Data/Others/AccountSettings/AccountSettingService.cs
+++ MASA.Blazor.Pro/Data/Others/AccountSettings/AccountSettingService.cs
@@ -4,15 +4,15 @@
     {
         public static AccountDto GetAccount() => new AccountDto("johndoe", "John Doe", "granger007@hogward.com", "Crystal Technologies");
 
-        public static List<CountryDto> GetCountryList()=> new()
+        public static List<CountryDto> GetCountryList() => new()
         {
-            new ("1", "USA"),
-            new ("2", "India"),
-            new ("3", "Canada"),
+            new("1", "USA"),
+            new("2", "India"),
+            new("3", "Canada"),
         };
 
-        public static InformationDto GetInformation() => new("", DateOnly.FromDateTime(DateTime.Now),"1","", 6562542568);
+        public static InformationDto GetInformation() => new("", DateOnly.FromDateTime(DateTime.Now), "1", "", 6562542568);
 
-        public static SocialDto GetSocial() => new("https://www.twitter.com","","", "https://www.linkedin.com","","");
+        public static SocialDto GetSocial() => new("https://www.twitter.com", "", "", "https://www.linkedin.com", "", "");
     }
 }
