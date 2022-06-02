54c443d0e83fca4bbeff553925add0aab8eec66f
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-06-02 18:36:09 +0800

    feat:update blazor version (#139)
    Co-authored-by: Mayue <mayue@lonsid.cn>

--- Masa.Blazor.Pro/Global/ServiceCollectionExtensions.cs
+++ Masa.Blazor.Pro/Global/ServiceCollectionExtensions.cs
@@ -3,8 +3,7 @@
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection AddGlobalForServer(this IServiceCollection services)
-        {     
-            services.AddMasaI18nForServer("wwwroot/i18n");
+        {
             var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new Exception("Get the assembly root directory exception!");
             services.AddNav(Path.Combine(basePath, $"wwwroot/nav/nav.json"));
             services.AddScoped<GlobalConfig>();
@@ -13,8 +12,7 @@
         }
 
         public static async Task<IServiceCollection> AddGlobalForWasmAsync(this IServiceCollection services, string baseUri)
-        {          
-            await services.AddMasaI18nForWasmAsync(Path.Combine(baseUri, $"i18n"));
+        {
             using var httpclient = new HttpClient();
             var navList = await httpclient.GetFromJsonAsync<List<NavModel>>(Path.Combine(baseUri, $"nav/nav.json")) ?? throw new Exception("please configure the Navigation!");
             services.AddNav(navList);
