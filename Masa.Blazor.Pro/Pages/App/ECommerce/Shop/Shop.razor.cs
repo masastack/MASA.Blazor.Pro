8b007ad813d8cbf423ae6cd4d287e13eca395d4c
wuweilai <30889371+15168440402@users.noreply.github.com>
2023-01-12 16:16:38 +0800

    fix shop page issues (#175)

--- Masa.Blazor.Pro/Pages/App/ECommerce/Shop/Shop.razor.cs
+++ Masa.Blazor.Pro/Pages/App/ECommerce/Shop/Shop.razor.cs
@@ -6,9 +6,20 @@
         readonly List<string> _categories = ShopService.GetCategortyList();
         readonly List<string> _brands = ShopService.GetBrandList();
         readonly ShopPage _shopData = new(ShopService.GetGoodsList());
+        string _multiRangeText = "All";
 
         [Inject]
         public NavigationManager Nav { get; set; } = default!;
+
+        string MultiRangeText
+        {
+            get => _multiRangeText;
+            set
+            {
+                _multiRangeText = value;
+                _shopData.MultiRange = _multiRanges.First(item => item.Text == value);
+            }
+        }
 
         protected override void OnInitialized()
         {
