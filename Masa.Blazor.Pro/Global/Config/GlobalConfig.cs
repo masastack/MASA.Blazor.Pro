805edeaa1ec500e009da5ce7806b077fe6c1baa6
Leon Liu <Leon.Engineer@hotmail.com>
2023-02-03 16:01:31 +0800

    Implement NavigationStyle config;

--- Masa.Blazor.Pro/Global/Config/GlobalConfig.cs
+++ Masa.Blazor.Pro/Global/Config/GlobalConfig.cs
@@ -4,88 +4,103 @@ namespace Masa.Blazor.Pro.Global;
 
 public class GlobalConfig
 {
-    #region Field
+	#region Field
 
-    private string? _pageMode;
-    private bool _expandOnHover;
-    private bool _navigationMini;
-    private string? _favorite;
-    private CookieStorage? _cookieStorage;
+	private string? _pageMode;
+	private bool _expandOnHover;
+	private bool _navigationMini;
+	private string? _favorite;
+	private CookieStorage? _cookieStorage;
+	private string? _navigationStyle;
 
-    #endregion
+	#endregion
 
-    #region Property
+	#region Property
 
-    public static string PageModeKey { get; set; } = "GlobalConfig_PageMode";
+	public static string PageModeKey { get; set; } = "GlobalConfig_PageMode";
 
-    public static string NavigationMiniCookieKey { get; set; } = "GlobalConfig_NavigationMini";
+	public static string NavigationStyleKey { get; set; } = "GlobalConfig_NavigationStyle";
 
-    public static string ExpandOnHoverCookieKey { get; set; } = "GlobalConfig_ExpandOnHover";
+	public static string NavigationMiniCookieKey { get; set; } = "GlobalConfig_NavigationMini";
 
-    public static string FavoriteCookieKey { get; set; } = "GlobalConfig_Favorite";
+	public static string ExpandOnHoverCookieKey { get; set; } = "GlobalConfig_ExpandOnHover";
 
-    public string PageMode
-    {
-        get => _pageMode ?? PageModes.PageTab;
-        set
-        {
-            _pageMode = value;
-            _cookieStorage?.SetItemAsync(PageModeKey, value);
-        }
-    }
+	public static string FavoriteCookieKey { get; set; } = "GlobalConfig_Favorite";
 
-    public bool NavigationMini
-    {
-        get => _navigationMini;
-        set
-        {
-            _navigationMini = value;
-            _cookieStorage?.SetItemAsync(NavigationMiniCookieKey, value);
-        }
-    }
+	public string PageMode
+	{
+		get => _pageMode ?? PageModes.PageTab;
+		set
+		{
+			_pageMode = value;
+			_cookieStorage?.SetItemAsync(PageModeKey, value);
+		}
+	}
 
-    public bool ExpandOnHover
-    {
-        get => _expandOnHover;
-        set
-        {
-            _expandOnHover = value;
-            _cookieStorage?.SetItemAsync(ExpandOnHoverCookieKey, value);
-        }
-    }
+	public string NavigationStyle
+	{
+		get => _navigationStyle ?? NavigationStyles.Flat;
+		set
+		{
+			_navigationStyle = value;
+			_cookieStorage?.SetItemAsync(NavigationStyleKey, value);
+		}
+	}
 
-    public string? Favorite
-    {
-        get => _favorite;
-        set
-        {
-            _favorite = value;
-            _cookieStorage?.SetItemAsync(FavoriteCookieKey, value);
-        }
-    }
 
-    #endregion
+	public bool NavigationMini
+	{
+		get => _navigationMini;
+		set
+		{
+			_navigationMini = value;
+			_cookieStorage?.SetItemAsync(NavigationMiniCookieKey, value);
+		}
+	}
 
-    public GlobalConfig(CookieStorage cookieStorage, IHttpContextAccessor httpContextAccessor)
-    {
-        _cookieStorage = cookieStorage;
-        if (httpContextAccessor.HttpContext is not null) Initialization(httpContextAccessor.HttpContext.Request.Cookies);
-    }
+	public bool ExpandOnHover
+	{
+		get => _expandOnHover;
+		set
+		{
+			_expandOnHover = value;
+			_cookieStorage?.SetItemAsync(ExpandOnHoverCookieKey, value);
+		}
+	}
 
-    #region event
+	public string? Favorite
+	{
+		get => _favorite;
+		set
+		{
+			_favorite = value;
+			_cookieStorage?.SetItemAsync(FavoriteCookieKey, value);
+		}
+	}
 
-    public delegate void GlobalConfigChanged();
+	#endregion
 
-    #endregion
+	public GlobalConfig(CookieStorage cookieStorage, IHttpContextAccessor httpContextAccessor)
+	{
+		_cookieStorage = cookieStorage;
+		if (httpContextAccessor.HttpContext is not null) Initialization(httpContextAccessor.HttpContext.Request.Cookies);
+	}
 
-    #region Method
+	#region event
 
-    public void Initialization(IRequestCookieCollection cookies)
-    {
-        _pageMode = cookies[PageModeKey];
-        _navigationMini = Convert.ToBoolean(cookies[NavigationMiniCookieKey]);
-        _expandOnHover = Convert.ToBoolean(cookies[ExpandOnHoverCookieKey]);
-        _favorite = cookies[FavoriteCookieKey];
-    }
-    #endregion
+	public delegate void GlobalConfigChanged();
+
+	#endregion
+
+	#region Method
+
+	public void Initialization(IRequestCookieCollection cookies)
+	{
+		_pageMode = cookies[PageModeKey];
+		_navigationStyle = cookies[NavigationStyleKey];
+		_navigationMini = Convert.ToBoolean(cookies[NavigationMiniCookieKey]);
+		_expandOnHover = Convert.ToBoolean(cookies[ExpandOnHoverCookieKey]);
+		_favorite = cookies[FavoriteCookieKey];
+	}
+	#endregion
 }
