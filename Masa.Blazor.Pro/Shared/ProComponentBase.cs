4e524682eb6331ea81cb465c132ab70766a230e7
capdiem <capdiem@live.com>
2023-03-20 23:59:06 +0800

    use next version of masa.blazor

--- Masa.Blazor.Pro/Shared/ProComponentBase.cs
+++ Masa.Blazor.Pro/Shared/ProComponentBase.cs
@@ -2,32 +2,14 @@
 
 public abstract class ProComponentBase : ComponentBase
 {
-    private string? _culture;
-
     [Inject]
     protected I18n? LanguageProvider { get; set; }
     
     [CascadingParameter(Name = "CultureName")]
-    protected string? Culture
-    {
-        get => _culture;
-        set
-        {
-            if (_culture != null && !Equals(_culture, value))
-            {
-                OnLanguageChanged();
-            }
-
-            _culture = value;
-        }
-    }
+    protected string? Culture { get; set; }
 
     protected string? T(string? key, params object[] args)
     {
         return LanguageProvider?.T(key, args: args);
-    }
-
-    protected virtual void OnLanguageChanged()
-    {
     }
 }
